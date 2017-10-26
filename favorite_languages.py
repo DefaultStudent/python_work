70f193d05731afd3358669cde8173cec8fc9825b
DefaultStudent <comrade_vodka@outlook.com>
2017-10-26 17:28:49 +0800

    add how to loop the dictionary

--- favorite_languages.py
+++ favorite_languages.py
@@ -6,3 +6,25 @@ favorite_languages = {
     }
 
 print ("Sarah's favorite language is " + favorite_languages['sarah'].title() + ".")
+
+# loop the dictionary
+
+for name, language in favorite_languages.items():
+    print (name.title() + "'s favorite language is " + language.title())
+
+
+for name in favorite_languages.keys():
+# for name in favorite_languages: is ok
+    print (name.title())
+
+
+# if in loop dictionary
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print (name.title())
+
+    if name in friends:
+        print ("Hi " + name.title() + ", I see your favorite language is " + favorite_languages[name].title() + "!")
+
+if 'erin' not in favorite_languages.keys():
+    print ("Erin,please take our poll")
---
+++ user.py
@@ -0,0 +1,9 @@
+user_o = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+    }
+
+for key, values in user_o.items():
+    print ("\nKey " + key)
+    print ("Value " + values)
