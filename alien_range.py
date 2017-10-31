ff64e946c8e9c00258b0b7065faca08e496cb6b9
DefaultStudent <comrade_vodka@outlook.com>
2017-10-31 22:07:08 +0800

    字典列表，添加了修改

--- alien_range.py
+++ alien_range.py
@@ -6,6 +6,17 @@ for alien_number in range(30):
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
 
+# 修改前五个外星人的字典数据
+for alien in aliens[0:5]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
 # 显示前五个外星人
 for alien in aliens[:5]:
     print (alien)
