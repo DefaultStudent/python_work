46a661e81dee3e215e1f801e2722e486d4469e8d
DefaultStudent <comrade_vodka@outlook.com>
2017-11-25 18:54:03 +0800

    使用类和实例

--- car.py
+++ car.py
@@ -15,11 +15,20 @@ class Car(object):
     def  read_dometer(self):
         print("This car has " + str(self.odometer_reading) + " miles on it.")
 
-    def update_odometer(self, message):
-        self.odometer_reading = message
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
 
 my_new_car = Car('audi', 'a4', 2016)
 print (my_new_car.get_descriptive_name())
 
-my_new_car.update_odometer(23)
+my_new_car.update_odometer(2300)
+my_new_car.read_dometer()
+
+my_new_car.increment_odometer(100)
 my_new_car.read_dometer()
