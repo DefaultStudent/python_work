b4147338b892b7cb418a1f3b6c9b94913d9aa07a
DefaultStudent <comrade_vodka@outlook.com>
2017-11-26 19:39:19 +0800

    导入类

--- car.py
+++ car.py
@@ -55,16 +55,16 @@ class ElecticCar(Car):
 
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
-my_tesla = ElecticCar('tesla', 'model s', 2016)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+# my_tesla = ElecticCar('tesla', 'model s', 2016)
+# print(my_tesla.get_descriptive_name())
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
 
-my_new_car = Car('audi', 'a4', 2016)
-print (my_new_car.get_descriptive_name())
-
-my_new_car.update_odometer(2300)
-my_new_car.read_dometer()
-
-my_new_car.increment_odometer(100)
-my_new_car.read_dometer()
+# my_new_car = Car('audi', 'a4', 2016)
+# print (my_new_car.get_descriptive_name())
+#
+# my_new_car.update_odometer(2300)
+# my_new_car.read_dometer()
+#
+# my_new_car.increment_odometer(100)
+# my_new_car.read_dometer()
