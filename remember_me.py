1a48628ac8a50a7be3214543b7dfb729b53b797f
DefaultStudent <comrade_vodka@outlook.com>
2017-12-01 22:54:47 +0800

    重构

--- remember_me.py
+++ remember_me.py
@@ -1,13 +1,31 @@
 import json
 
-filename = 'username.json'
-try:
-    with open(filename) as file_object:
-        username = json.load(file_object)
-except FileNotFoundError:
+def get_stored_username():
+
+    filename = 'username.json'
+    try:
+        with open(filename) as file_object:
+            username = json.load(file_object)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+
     username = input("What is your name? ")
+    filename = 'username.json'
     with open(filename, 'w') as file_object:
         json.dump(username, file_object)
+    return username
+
+def greet_user():
+
+    username = get_stored_username()
+    if username:
+        print("Welcome bcak, " + username + "!")
+    else:
+        username = get_new_username()
         print("We'll remember you when you come back, " + username + "!")
-else:
-    print("Welcome back, " + username + "!")
+
+greet_user()
